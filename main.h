e79ed61a79ac4e354d5d61576125d45eacde6943
Jaber The BobCatTeam <jaberlama123@gmail.com>
2020-03-19 20:23:29 +0200

    Bases files
    Basically it's just the Module sdk

---
+++ main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MODULE_NAME         "Streamer Module"
+#define MODULE_AUTHOR       "Jaber"
+#define MODULE_VERSION      1.0
+
+#include <string.h>
+#include "Common.h"
+#include "include\ILuaModuleManager.h"
